84487b2b6640b6f65a095a9274a9a812663f7761
Vaidehi <vaidehisawant2812@gmail.com>
2024-02-26 21:52:25 +0530

    Added Airport model and configured associations in Airport and City tables

--- src/models/airport.js
+++ src/models/airport.js
@@ -3,30 +3,27 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
+  class Airport extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      this.hasMany(models.Airport,
-        {
-          foreignKey : 'cityId'
-        })
+      this.belongsTo(models.City,{
+        foreignKey : 'cityId',
+        onDelete : 'CASCADE'
+      })
     }
   }
-  City.init({
-    name: {
-      type:DataTypes.STRING,
-      allowNull :false,
-      unique:true
-    },
+  Airport.init({
+    name: {type : DataTypes.STRING,allowNull : false},
+    address: {type : DataTypes.STRING,allowNull : false},
+    cityId: DataTypes.INTEGER
   }, 
-    {
+  {
     sequelize,
-    modelName: 'City',
+    modelName: 'Airport',
   });
-  return City;
+  return Airport;
 };
